c86bc1f6e66c32164c2d4b8042e5d6582dc2c0b5
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2025-12-05 14:13:18 -0300

    Update Trabalho.c

--- Trabalho.c
+++ Trabalho.c
@@ -28,3 +28,17 @@ long co_curso;
 double nt_ger;
 double nt_ce; 
 } Arq3Record;
+
+static Arq1Record **arq1 = NULL;
+static size_t arq1_count = 0;
+static size_t arq1_capacity = 0;
+
+
+static Arq3Record *arq3 = NULL;
+static size_t arq3_count = 0;
+static size_t arq3_capacity = 0;
+
+static Arq1Record **idx_by_course = NULL; 
+static Arq1Record **idx_by_ies = NULL; 
+static Arq1Record **idx_by_uf = NULL; 
+static Arq1Record **idx_by_group = NULL; 
