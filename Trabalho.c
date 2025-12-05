fc87878b872a0d35980cb61274bc3f0e6a8256f5
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2025-12-05 10:59:25 -0300

    Update Trabalho.c

--- Trabalho.c
+++ Trabalho.c
@@ -1,3 +1,24 @@
-//
-// Created by diogo on 28/11/2025.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int ano;
+    int cod_curso;
+    int cod_ies;
+    int cod_categoria;
+    int cod_org_acad;
+    int cod_grupo;
+    int cod_modalidade;
+    int cod_municipio;
+    int cod_uf;
+    int cod_regiao;
+} DadosCurso;
+
+typedef struct {
+    int ano;
+    int cod_curso;
+    float nota_geral;
+    float nota_especifica;
+} DadosNotas;
+
